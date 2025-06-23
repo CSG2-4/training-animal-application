c2e7a0c7e33c63fdaca92ea5b963dccefa224097
taakaike <dasbeste6613@gmail.com>
2025-06-23 13:49:49 +0900

    kadai_2

---
+++ src/main/java/com/example/training_animal_application/repository/AnimalRepository.java
@@ -0,0 +1,9 @@
+package com.example.training_animal_application.repository;
+
+import com.example.training_animal_application.model.Animal;
+
+import java.util.List;
+
+public interface AnimalRepository {
+    List<Animal> fetch();
+}
---
+++ src/main/java/com/example/training_animal_application/repository/impl/AnimalRepositoryImpl.java
@@ -0,0 +1,32 @@
+package com.example.training_animal_application.repository.impl;
+
+import com.example.training_animal_application.entity.AnimalEntity;
+import com.example.training_animal_application.model.Animal;
+import com.example.training_animal_application.model.vo.Weight;
+import com.example.training_animal_application.repository.AnimalRepository;
+import com.example.training_animal_application.store.AnimalStore;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class AnimalRepositoryImpl implements AnimalRepository {
+
+    private final AnimalStore animalStore;
+
+    @Override
+    public List<Animal> fetch() {
+        List<AnimalEntity> entities = animalStore.findAll();
+        return entities.stream().map(this::toModel).toList();
+    }
+
+    private Animal toModel(AnimalEntity entity) {
+        Animal model = new Animal();
+        model.setAnimalId(entity.getAnimalId());
+        model.setName(entity.getName());
+        model.setWeight(new Weight(entity.getWeight()));
+        return model;
+    }
+}
