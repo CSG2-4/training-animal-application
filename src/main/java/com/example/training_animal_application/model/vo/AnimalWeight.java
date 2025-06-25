449e3024860f5da3fc10cbd514ca0092cc6c7239
ReitoKuga <reito.kuga@ardito.jp>
2025-06-25 18:25:43 +0900

    フィードバックを受けた点を元に写経し修正

--- src/main/java/com/example/training_animal_application/model/vo/AnimalWeight.java
+++ src/main/java/com/example/training_animal_application/model/vo/AnimalWeight.java
@@ -2,25 +2,25 @@ package com.example.training_animal_application.model.vo;
 
 import lombok.Value;
 
-@Value
-public class AnimalWeight {
-
-  private static final String UNIT = "Kg";
-  int value;
-
-  public String getWeight() {
-    return value + UNIT;
+// Weightを継承しているので、Weightクラスにweightの比較メソッドを用意すれば「檻の最大重量と動物の体重で比較」が実現できる
+public class AnimalWeight extends Weight {
+  public AnimalWeight(int value) {
+    super(value);
   }
 
-  public int calcCorrectedWeight(int weight) {
-    if (weight >= 100) {
-      return weight + 10;
-    } else if (weight >= 50) {
-      return weight + 3;
-    } else if (weight >= 10) {
-      return weight + 1;
-    } else {
-      return weight;
+  // 戻り値をintではなくweightにすることで、補正後の体重を体重以外の用途で利用されることを禁止できる。
+  // 補正するメソッドはサブクラスにも持たせているので「動物の体重のみ補正をかけれて」を実現
+  public Weight getHouseWeight() {
+    int v = getValue();
+    if (v < 10) {
+      return new Weight(v);
     }
+    if (v < 50) {
+      return new Weight(v + 1);
+    }
+    if (v < 100) {
+      return new Weight(v + 3);
+    }
+    return new Weight(v + 10);
   }
 }
